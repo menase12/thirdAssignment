8fd57e62c6e06772fd32416cf0935c9b6f3e9faa
student <behabtuhiwot@gmail.com>
2017-10-02 16:26:38 -0400

    assignment of code for yourself

---
+++ src/CodeMyself.java
@@ -0,0 +1,11 @@
+public class CodeMyself
+{
+
+        public static void main(String arg [])
+        {
+        int age;//declaring an intiger variable//
+        age=25;
+        System.out.println("My name is Yonatan Zewde");
+        System.out.println("I am " +age+ " years old.");
+    }
+}
